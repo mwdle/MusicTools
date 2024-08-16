faecd155150dc92652da7b5609c1ac5c4215f43a
Michael Wadley <michael@mwdle.com>
2024-08-16 15:28:18 -0600

    Initial Commit

---
+++ generatePlaylist.py
@@ -0,0 +1,37 @@
+import os
+import argparse
+import re
+from mutagen.mp3 import MP3
+from mutagen.oggvorbis import OggVorbis
+
+def get_duration(file_path):
+    if file_path.lower().endswith('.mp3'):
+        audio = MP3(file_path)
+    if file_path.lower().endswith('.ogg'):
+        audio = OggVorbis(file_path)
+    return audio.info.length
+
+def generate_m3u_playlist(playlist_directory, music_directory, output_file, exclusions):
+    with open(f"{music_directory}{os.path.sep}{output_file}", 'w', encoding='utf-8') as playlist:
+        playlist.write("#EXTM3U\n")
+        for root, _, files in os.walk(playlist_directory):
+            for file in files:
+                if file.lower().endswith('.ogg') or file.lower().endswith('.mp3'):
+                    song_path = os.path.join(root, file)
+                    if exclusions != "" and re.search(exclusions, song_path):
+                        continue
+                    song_length = int(get_duration(song_path))
+                    relative_path = os.path.relpath(song_path, music_directory)
+                    playlist.write(f"#EXTINF:{song_length}, {file[:-4]}\n{relative_path}\n");
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="This script allows you to generate an m3u playlist given a folder containing music and/or subfolders with music. The following example generates a playlist called Jazz.m3u in /home/user/Media/Music containing all songs in the Jazz folder/subfolders: python3 generatePlaylist.py '/home/user/Media/Music/Jazz' '/home/user/Media/Music' 'Jazz.m3u'")
+    parser.add_argument('playlist_directory', type=str, help="Path to the folder containing music/subfolders that you want to be added to the playlist.")
+    parser.add_argument('music_directory', type=str, help="Path to the folder containing audio files and/or subfolders with audio files. This is the location where you want to create the playlist file - the 'root' folder of your music library.")
+    parser.add_argument('output_file', type=str, help="Name of m3u file (including .m3u extension). Example: 'Jazz.m3u'")
+    parser.add_argument('exclusions', type=str, nargs='?', default="", help="(Optional) A regular expression to exclude specific folders and filenames from being added to the playlist. Leaving this empty will not exclude anything. The following example generates a playlist called 'Everything Except Jazz' containing all songs in the music folder except those matching 'Jazz': python3 generatePlaylist.py '/home/user/Media/Music' '/home/user/Media/Music' 'Everything Except Jazz.m3u' 'Jazz'")
+    
+    args = parser.parse_args()
+    
+    generate_m3u_playlist(args.playlist_directory, args.music_directory, args.output_file, args.exclusions)
+    print(f"Playlist generated: {args.music_directory}{os.path.sep}{args.output_file}")
---
+++ normalizeNames.py
@@ -0,0 +1,62 @@
+import os
+import argparse
+
+illegal_chars = {'?', ':', '"', '*', '!'}
+
+def rename_item(root, old_path, new_name):
+    new_path = os.path.join(root, new_name)
+    if old_path != new_path:
+        os.rename(old_path, new_path)
+        print(f'Renamed item: {old_path} -> {new_path}')
+
+def replace_colon(old_name, illegal_char):
+    new_name = old_name
+    preceding_char = old_name.index(illegal_char)-1
+    succeeding_char = old_name.index(illegal_char)+1
+    if preceding_char < len(old_name) and preceding_char >= 0 and old_name[preceding_char] != ' ':
+        if succeeding_char < len(old_name) and succeeding_char >= 0 and old_name[succeeding_char] != ' ':
+            new_name = new_name.replace(illegal_char, '-')
+        else: new_name = new_name.replace(illegal_char, ' -')
+    elif succeeding_char < len(old_name) and succeeding_char >= 0 and old_name[succeeding_char] != ' ':
+        new_name = new_name.replace(illegal_char, '- ')
+    else: new_name = new_name.replace(illegal_char, '-')
+    return new_name
+
+# You can implement custom logic here for handling specific illegal characters.
+def repair_illegal_name(old_name, illegal_chars):
+    new_name = old_name
+    for illegal_char in illegal_chars:
+        if illegal_char in old_name:
+            if illegal_char == ':':
+                new_name = replace_colon(old_name, illegal_char)
+            elif illegal_char == '"':
+                new_name = new_name.replace(illegal_char, "'")
+            else:
+                new_name = new_name.replace(illegal_char, '')
+    if new_name.endswith("..."):
+        new_name = new_name[:-3]
+    if new_name.endswith('.'):
+        new_name = new_name[:-1]
+    return new_name
+
+def rename_folders(root_folder):
+    for root, dirnames, _ in os.walk(root_folder):
+        for dirname in dirnames:
+            if any(illegal_char in dirname for illegal_char in illegal_chars) or dirname.endswith('.'):
+                new_dirname = repair_illegal_name(dirname, illegal_chars)
+                rename_item(root, os.path.join(root, dirname), new_dirname)
+
+def rename_files(root_folder):
+    for root, _, filenames in os.walk(root_folder):
+        for filename in filenames:
+            if any(illegal_char in filename for illegal_char in illegal_chars) or filename.endswith('.'):
+                new_name = repair_illegal_name(filename, illegal_chars)
+                rename_item(root, os.path.join(root, filename), new_name)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="This script allows you to quickly and easily replace or remove 'illegal' characters from filenames and foldernames in a specific a folder and subfolders that break sync compatibility between devices using different filesystem conventions. A practical use of this application is bulk renaming stored music folders/files to enable portability and syncing of a music library across devices without file system compatibility issues. This program does *NOT* modify music metadata, just filenames/foldernames. By default, this application will intelligently replace colons ':' with dashes '-' and proper surrounding spacing, replace quotation marks '\"' with single quotes ', remove trailing periods '.', and remove any other characters defined in the illegal characters list at the top of the normalizeNames.py file. All of the aforementioned behavior can be easily overridden/modified in the repair_illegal_name() method and illegal characters list.")
+    parser.add_argument('root_folder', type=str, help="Path to folder containing music/subfolders to repair names in.")
+    args = parser.parse_args()
+    rename_files(args.root_folder)
+    rename_folders(args.root_folder)
+    print("Finished! Exiting . . .")
